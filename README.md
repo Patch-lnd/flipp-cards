eece870b6fa65bd988f18e6e47d6267f4b194445
Djeumeni Patchepia Arthur Samuel <djeumenipatchepiaarthursamuel@gmail.com>
2025-07-26 14:15:03 +0100

    first part of my flipp card game on going
    The iconic flipp-card remake

--- README.md
+++
@@ -1,2 +0,0 @@
-# flipp cards
- colourful version
---
+++ index.js
@@ -0,0 +1,5 @@
+const gridContainer = document.querySelectioner(".grid-container")
+let cards = [];
+let firstCard, secondCard;
+let lockBoard =  false;
+let score = 0;
